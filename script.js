aec6aac01978d21e3a209d8b45478a3a4fa78525
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-08-01 08:54:40 +0300

    fixed navbar, responsive, html code, meta author

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-// toggle scroll sections
-let menuIcon = document.querySelector("#menu");
-let navbar = document.querySelector(".navbar");
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("fa-xmark");
-  navbar.classList.toggle("active");
-};
+"use strict";
 
-let sections = document.querySelectorAll("section");
-let navlinks = document.querySelectorAll("header nav a");
+const menuIcon = document.getElementById("menu");
+const navbar = document.querySelector(".navbar");
+const homeLink = document.querySelector('.navbar a[href="#home"]');
+const navLinks = document.querySelectorAll(".navbar a");
+const sections = document.querySelectorAll("body>section");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  menuIcon.classList.toggle("fa-xmark");
+});
+
+// Add event listener for the "Home" link
+homeLink.addEventListener("click", () => {
+  closeNavbar();
+});
+
+// Add event listener for all navigation links
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeNavbar();
+  });
+});
+
+// Function to close the navbar on small screens
+function closeNavbar() {
+  navbar.classList.remove("active");
+}
 
 // Supposons que vous ayez déjà défini les variables sections et navlinks ailleurs dans le code.
 
@@ -20,7 +39,7 @@ window.onscroll = () => {
 
     if (top >= offset && top < offset + height) {
       // active navbar links
-      navlinks.forEach((links) => {
+      navLinks.forEach((links) => {
         links.classList.remove("active");
         document
           .querySelector('header nav a[href*="' + id + '"]')
@@ -37,4 +56,5 @@ window.onscroll = () => {
   // remove toggle icon and navbar
   menuIcon.classList.remove("fa-xmark");
   navbar.classList.remove("active");
+  console.log('Y');
 };
